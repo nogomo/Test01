81c4341ceb6dc179b42b9a5a507a6fd059d8b514
nogomo <85296467+nogomo@users.noreply.github.com>
2021-07-19 17:54:45 +0300

    Update IndexController.java

--- src/main/java/com/viki3d/spring/front/controllers/IndexController.java
+++ src/main/java/com/viki3d/spring/front/controllers/IndexController.java
@@ -1,17 +1,19 @@
 package com.viki3d.spring.front.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * IndexController.
+ *
+ * @author User
+ */
 @RestController
 public class IndexController {
 
-	@GetMapping(path = "/")
-	public String index() {
-		
-		
-		return "123";
-	}
-	
+  @GetMapping(path = "/")
+  public String index() {
+    return "123";
+  }
+
 }
