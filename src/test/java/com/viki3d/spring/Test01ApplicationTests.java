f2045a7b18debcb20809854385869ed5da36f069
nogomo <85296467+nogomo@users.noreply.github.com>
2021-07-19 17:43:21 +0300

    Update Test01ApplicationTests.java

--- src/test/java/com/viki3d/spring/Test01ApplicationTests.java
+++ src/test/java/com/viki3d/spring/Test01ApplicationTests.java
@@ -2,6 +2,7 @@ package com.viki3d.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Test01ApplicationTests {
